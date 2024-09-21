800b5f613d744bd3d0c412f57367a2ea2b77bf25
Stephen Stage <stagestephen1@gmail.com>
2024-09-21 10:49:04 -0500

    spring web default package, lombok, and readme

---
+++ src/main/java/org/group9/Group9Application.java
@@ -0,0 +1,13 @@
+package org.group9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group9Application.class, args);
+	}
+
+}
